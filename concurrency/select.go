29fe60f0d0af87709751c5684057527d48943b46
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2019-03-02 09:59:47 +0700

    add select directive example

---
+++ concurrency/select.go
@@ -0,0 +1,36 @@
+package concurrency
+
+import (
+	"fmt"
+	"time"
+)
+
+func Select() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "C1"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "C2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			}
+		}
+	}()
+}
--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
-	var c chan string = make(chan string)
-	go concurrency.Pinger(c)
-	go concurrency.Ponger(c)
-	go concurrency.Printer(c)
+
+	//Ping Pong
+	// var c chan string = make(chan string)
+	// go concurrency.Pinger(c)
+	// go concurrency.Ponger(c)
+	// go concurrency.Printer(c)
+
+	concurrency.Select()
 
 	var input string
 	fmt.Scanln(&input)
